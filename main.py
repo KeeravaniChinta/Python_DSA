943cc99d165ad06efe1b5df3c9821b63c56c3aec
krishnachintakeeravani@gmail.com <krishnachintakeeravani@gmail.com>
2026-07-23 12:29:01 +0530

    day1

---
+++ main.py
@@ -0,0 +1,52 @@
+#print("Keeravani\n"*3)
+#print(r"C:\new\test")
+#print("C:\\new\\test") 
+#print(int(float("1.2"))) 
+
+
+# n=int(input("Enter a number:"))
+# if n%2==0:
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+
+# dict={
+#     "name":"Keeravani",
+#     "gender":"female",
+#     "age":20,
+#     "courses":["python","java","datascience"]
+# }
+# #dict["name"]="Sri"
+# dict.update(name="Kokila")
+# print(dict)
+# print(dict.keys())
+# print(dict.items())
+# print(dict.values())
+# print(dict["age"])
+# print(dict.get("name"))
+
+
+# l=[1,2,3,4,5]
+# l.remove(3)
+# l.pop()
+# print(l)
+
+
+l=[1,2]
+l.append(3)
+print(l)
+x=[]
+print(len(x))
+x.append(l)
+print(x)
+print(len(x),"len(x)")
+print(len(x[0]),"len(x[0])")
+m=[4,5,6]
+x.append(m)
+print(x)
+t=[]
+t.append(x)
+t.append([100,200,300])
+print(t)
+print(t[0])
