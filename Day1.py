5090bcab006ce7ead021ebdc934c4c5dca2c9b48
krishnachintakeeravani@gmail.com <krishnachintakeeravani@gmail.com>
2026-08-27 14:22:18 +0530

    Add Day1 DSA programs

--- Day1.py
+++ Day1.py
@@ -1,49 +1,86 @@
-class Node:
+# #singly linked list
+# class Node:
+#     def __init__(self,data):
+#         self.data=data
+#         self.next=None
+# class LinkedList:
+#     def __init__(self):
+#         self.head=None
+#     def add_node(self,data):
+#         node=Node(data)
+#         if not self.head:
+#             self.head=node
+#         else:
+#             current=self.head
+#             while current.next is not None:
+#                 current=current.next
+#             current.next=node
+#     def delete(self,data):
+#         if self.head.data==data:
+#             self.head=self.head.next
+#         else:
+#             current=self.head
+#             while current.next is not None:
+#                 if current.next.data==data:
+#                     current.next=current.next.next
+#                     print("Deleted")
+#                     return
+#                 current=current.next
+#             print("Not Found")
+#     def display(self):
+#         current=self.head
+#         while current is not None:
+#             print(current.data,"->",end=" ")
+#             current=current.next
+#         print("None")
+#     def insert_at_beginning(self,data):
+#         node=Node(data)
+#         if not self.head:
+#             self.head=node
+#         else:
+#             node.next=self.head
+#             self.head=node
+#     def insert_at_kth_position(self,data,k):
+#         node=Node(data)
+#         if not self.head:
+#             self.head=node
+#         else:
+#             current=self.head
+#             count=1
+#             while current is not None and k>1:
+#                 current=current.next
+#                 count+=1
+#                 if count==k-1:
+#                     node.next=current.next
+#                     current.next=node
+#                     print("Inserted at kth position")
+#                     return
+#             print("Not Found")
+#     def reverse(self):
+#         prev = None
+#         current = head
+#         while current:
+#             next_node = current.next
+#             current.next = prev
+#             prev = current
+#             current = next_node
+#         self.head=prev
+
+# # ll1=LinkedList()
+# # ll1.add_node(10)
+# # ll1.add_node(20)
+# # ll1.add_node(30)
+# # ll1.add_node(40)
+# # ll1.add_node(50)
+# # ll1.display()
+
+
+#doubly linked list
+class DNode:
     def __init__(self,data):
         self.data=data
         self.next=None
-class LinkedList:
+        self.prev=None
+class DLinkedList:
     def __init__(self):
         self.head=None
-    def add_node(self,data):
-        node=Node(data)
-        if not self.head:
-            self.head=node
-        else:
-            current=self.head
-            while current.next is not None:
-                current=current.next
-            current.next=node
-    def delete(self,data):
-        if self.head.data==data:
-            self.head=None
-        else:
-            current=self.head
-            while current.next is not None:
-                if current.next.data==data:
-                    current.next=current.next.next
-                    print("Deleted")
-                    return
-                current=current.next
-            print("Not Found")
-    def display(self):
-        current=self.head
-        while current is not None:
-            print(current.data,"->",end=" ")
-            current=current.next
-        print("None")
-    def insert_at_beginning(self,data):
-        node=Node(data)
-        if not self.head:
-            self.head=node
-        else:
-            node.next=self.head
-            self.head=node
-
-# ll1=LinkedList()
-# ll1.add_node(10)
-# ll1.add_node(20)
-# ll1.add_node(30)
-# ll1.add_node(40)
-# ll1.add_node(50)
-# ll1.display()
